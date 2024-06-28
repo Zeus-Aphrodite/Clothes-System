b8bca570ebe83747386a3c50eab1e8054c71044e
Zeus-Aphrodite <zeusa1398@gmail.com>
2024-06-28 14:17:40 +0900

    hook

--- src/hooks/debounce.js
+++ src/hooks/debounce.js
@@ -7,6 +7,5 @@ export function useDebounce(value, delay = 300) {
         const handler = setTimeout(() => setDebounced(value), delay)
         return () => clearTimeout(handler)
     }, [value, delay])
-
     return debounced
 }
